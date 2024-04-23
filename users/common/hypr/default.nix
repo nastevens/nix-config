1f661cf7eed22f21737fc4c69098a188d54941ad
Nick Stevens <nick@bitcurry.com>
2024-04-23 17:11:40 -0500

    feat(hypr): use hyprpaper, cleanup hyprland config

--- users/common/hypr/default.nix
+++ users/common/hypr/default.nix
@@ -2,14 +2,14 @@
 {
   imports = [
     inputs.hypridle.homeManagerModules.default
-    # inputs.hyprlock.homeManagerModules.default
+    inputs.hyprpaper.homeManagerModules.default
   ];
+
   home.packages = with pkgs; [
     dunst
     wl-clipboard
     shotman
     waybar
-    swww # Wallpaper
     bibata-cursors
     mate.caja
     pamixer
@@ -17,6 +17,7 @@
     xdg-desktop-portal-hyprland
     eww-wayland
   ];
+
   wayland.windowManager.hyprland = {
     enable = true;
     package = pkgs-unstable.hyprland;
@@ -24,21 +25,22 @@
     # Could use `settings` instead https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.hyprland.settings
     extraConfig = builtins.readFile ./hyprland.conf;
   };
+
   services.hypridle = {
     enable = true;
-    #lockCmd = "pidof hyprlock || hyprlock";
-    #beforeSleepCmd = "${pkgs.systemd}/bin/loginctl lock-session";
-    #afterSleepCmd = "hyprctl dispatch dpms on";
     listeners = [
-      # {
-      #   timeout = 600;
-      #   onTimeout = "${pkgs.systemd}/bin/loginctl lock-session";
-      # }
       {
         timeout = 600;
         onTimeout = "hyprctl dispatch dpms off";
         onResume = "hyprctl dispatch dpms on";
       }
     ];
+  };
+
+  services.hyprpaper = {
+    enable = true;
+    preloads = [ "/etc/moon_landing.jpg" ];
+    wallpapers = [ "/etc/moon_landing.jpg" ];
+    splash = true;
   };
 }
