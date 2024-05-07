9baca3bfecf1b44389c1b7c8b27117a9cbac950e
Nick Stevens <nick@bitcurry.com>
2024-05-07 08:41:12 -0500

    feat(hypr): add hyprland cachix cache

--- hosts/bakugo/configuration.nix
+++ hosts/bakugo/configuration.nix
@@ -88,7 +88,11 @@
     ];
   };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
