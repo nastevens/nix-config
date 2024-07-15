1db737ca2785c341f0f99e0437433c3b436b9c43
Nick Stevens <nick@bitcurry.com>
2024-07-15 15:55:26 -0500

    ssh: add key from minion VM

--- users/default.nix
+++ users/default.nix
@@ -31,6 +31,7 @@ in
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAQhKRLdPbO7wXpUKkskEoeZMxjcv3STbTU5RARaSBsA notnotlinux-20240206"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEkQY+0M/FaMRqOlilm/YrjFNqskJ5umtRhrBVKB5MuO samsung-sm-x200-connectbot-20240613"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICXYe9Zo078ay3wTyfJDAfvYt6ut6cHRj7UykZRoaGWw samsung-sm-x200-termius20240613"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPpAOnlrWsvVjeDXjC0WzOgGHu5h7n0dAYE5I/gm835M nick@minion22"
       ];
     };
   };
