955370a51185872c3f37bc6d24240a7665840324
Nick Stevens <nick@bitcurry.com>
2024-08-04 13:30:52 -0500

    sshd: enforce strict login failure limits

--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -11,6 +11,11 @@ in
       KbdInteractiveAuthentication = false;
       PasswordAuthentication = false;
       PermitRootLogin = "no";
+
+      # I strictly limit users and auth methods, so the only way a legitimate
+      # login attempt would get caught is if I forget to load my ssh key.
+      # Therefore I can be pretty draconian with the failure penalties.
+      PerSourcePenalties = "max:24h crash:1h authfail:5m noauth:5m";
     };
   };
 }
