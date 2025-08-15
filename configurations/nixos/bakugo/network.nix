26bfc88773c39a308125a25b5f33a71d83f98fb5
Nick Stevens <nick@bitcurry.com>
2025-08-15 10:35:58 -0500

    net: updated to SFP+ NIC

--- configurations/nixos/bakugo/network.nix
+++ configurations/nixos/bakugo/network.nix
@@ -7,31 +7,24 @@
     useDHCP = false;
   };
 
-  # Note on commented sections: on 2025-08-09 a lightning storm killed the NIC
-  # on bakugo, so I am temporarily using a USB NIC. It also killed the port
-  # bakugo was connected to on the Dream Machine, so VLAN for IoT network is
-  # disabled.
   systemd.network = {
     enable = true;
 
-    # netdevs = {
-    #   "20-vlan3" = {
-    #     netdevConfig = {
-    #       Name = "vlan3";
-    #       Kind = "vlan";
-    #       Description = "IoT Network";
-    #     };
-    #     vlanConfig.Id = 3;
-    #   };
-    # };
+    netdevs = {
+      "20-vlan3" = {
+        netdevConfig = {
+          Name = "vlan3";
+          Kind = "vlan";
+          Description = "IoT Network";
+        };
+        vlanConfig.Id = 3;
+      };
+    };
 
     networks = {
       "30-wired" = {
-        # Temporary USB NIC
-        matchConfig.Name = "enp7s0f1u1";
-
-        # matchConfig.Name = "enp6s0";
-        # vlan = [ "vlan3" ];
+        matchConfig.Name = "enp5s0np0";
+        vlan = [ "vlan3" ];
         networkConfig = {
           DHCP = "ipv4";
           LinkLocalAddressing = "no";
@@ -40,15 +33,15 @@
         dhcpV4Config.UseDomains = "yes";
       };
 
-      # "40-vlan3" = {
-      #   matchConfig.Name = "vlan3";
-      #   networkConfig = {
-      #     DHCP = "ipv4";
-      #     LinkLocalAddressing = "no";
-      #     ConfigureWithoutCarrier = true;
-      #   };
-      #   linkConfig.RequiredForOnline = "no";
-      # };
+      "40-vlan3" = {
+        matchConfig.Name = "vlan3";
+        networkConfig = {
+          DHCP = "ipv4";
+          LinkLocalAddressing = "no";
+          ConfigureWithoutCarrier = true;
+        };
+        linkConfig.RequiredForOnline = "no";
+      };
     };
   };
 
