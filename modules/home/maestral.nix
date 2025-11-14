fa43f20041eaa1cb0a63974d73ae8b0761dfb701
Nick Stevens <nick@bitcurry.com>
2025-11-13 19:35:15 -0600

    maestral: add daemon systemd unit

--- modules/home/maestral.nix
+++ modules/home/maestral.nix
@@ -1,7 +1,16 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib.meta) getExe;
   maestral = pkgs.maestral;
+  failAction = builtins.replaceStrings [ "\n" ] [ " " ] ''
+    if [ ''${SERVICE_RESULT} != success ]; then
+      notify-send Maestral 'Daemon failed: ''${SERVICE_RESULT}'
+    fi
+  '';
 in
 {
   home.packages = [
@@ -10,4 +19,26 @@ in
   programs.zsh.initContent = ''
     eval "$(${getExe maestral} completion zsh)"
   '';
+  systemd.user.services."maestral-daemon" = {
+    Unit = {
+      Description = "Maestral daemon";
+    };
+    Service = {
+      Type = "notify";
+      ExecStart = ''
+        ${getExe maestral} start --foreground --config-name="maestral"
+      '';
+      WatchdogSec = 30;
+      ExecStop = ''
+        ${getExe maestral} stop --config-name="maestral"
+      '';
+      ExecStopPost = ''
+        ${pkgs.runtimeShell} -c "${failAction}"
+      '';
+      Environment = "PYTHONOPTIMIZE=2 LC_CTYPE=UTF-8";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
