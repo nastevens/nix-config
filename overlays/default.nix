5db973659460280e5efb2fa6f60b591652c055e1
Nick Stevens <nick@bitcurry.com>
2026-01-06 15:31:11 -0600

    fix: hyprland window rules need regex now

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,7 +6,6 @@ let
   packages = self + /packages;
 in
 (final: prev: {
-  # Downgrade to avoid 1Password GUI glitch - remove @ 8.11.18 release
   git-utils = final.callPackage "${packages}/git-utils" { };
   open-slug = final.callPackage "${packages}/open-slug" { };
   prusaslicer-rename = final.callPackage "${packages}/prusaslicer-rename" { };
