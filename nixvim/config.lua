b20a84bae93f021b3f48a4add0207fc103940497
Nick Stevens <nick@bitcurry.com>
2026-05-10 12:36:33 -0500

    chore: reformat nixvim lua config

--- nixvim/config.lua
+++ nixvim/config.lua
@@ -1,6 +1,5 @@
 -- TODO: How do I make separate Lua modules for stuff like the codeoptions?
 
-
 local utils = {}
 
 utils.unload_lua_namespace = function(prefix)
@@ -307,42 +306,39 @@ do
 
         -- Adds a new project
         vim.keymap.set("n", "<leader>wa", function()
-            vim.ui.input(
-                { prompt = "New project name: " },
-                function(input)
-                    if input then
-                        local path = vim.call("vimwiki#vars#get_wikilocal", "path")
-                        local ext = vim.call("vimwiki#vars#get_wikilocal", "ext")
-                        local ft_is_vw = vim.call("vimwiki#u#ft_is_vw")
-                        local wiki_file = path .. "projects/" .. input .. ext
+            vim.ui.input({ prompt = "New project name: " }, function(input)
+                if input then
+                    local path = vim.call("vimwiki#vars#get_wikilocal", "path")
+                    local ext = vim.call("vimwiki#vars#get_wikilocal", "ext")
+                    local ft_is_vw = vim.call("vimwiki#u#ft_is_vw")
+                    local wiki_file = path .. "projects/" .. input .. ext
 
-                        -- When we return put the cursor on the link we create (one line
-                        -- below the current line)
-                        local pos = vim.fn.getpos(".")
-                        pos[2] = pos[2] + 1
-                        local prev_link = {
-                            vim.call("vimwiki#path#current_wiki_file"),
-                            pos,
-                        }
+                    -- When we return put the cursor on the link we create (one line
+                    -- below the current line)
+                    local pos = vim.fn.getpos(".")
+                    pos[2] = pos[2] + 1
+                    local prev_link = {
+                        vim.call("vimwiki#path#current_wiki_file"),
+                        pos,
+                    }
 
-                        -- Add link below current cursor position
-                        vim.fn.append(vim.fn.line("."), project_link(input))
+                    -- Add link below current cursor position
+                    vim.fn.append(vim.fn.line("."), project_link(input))
 
-                        -- Write out the template and open it
-                        if vim.fn.filereadable(wiki_file) == 0 then
-                            vim.fn.writefile(project_template(input), wiki_file)
-                        end
-                        vim.call(
-                            "vimwiki#base#edit_file",
-                            "edit",
-                            wiki_file,
-                            "",
-                            prev_link,
-                            ft_is_vw
-                        )
+                    -- Write out the template and open it
+                    if vim.fn.filereadable(wiki_file) == 0 then
+                        vim.fn.writefile(project_template(input), wiki_file)
                     end
+                    vim.call(
+                        "vimwiki#base#edit_file",
+                        "edit",
+                        wiki_file,
+                        "",
+                        prev_link,
+                        ft_is_vw
+                    )
                 end
-            )
+            end)
         end)
     end
 end
